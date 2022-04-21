b7091e5904a056dc19f86ed17275d7cac254e69e
Amy Churchwell <amychurchwell@protonmail.com>
2022-04-21 16:16:16 -0700

    add md library, some indieweb, fill out resume.

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,41 @@
-const fs = require('fs')
-const NOT_FOUND_PATH = '_site/404.html'
-require('dotenv').config()
-
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/css/');
-  eleventyConfig.addWatchTarget('./src/css/');
+    let options = {
+        html: true,
+        breaks: true,
+        linkify: true,
+    }
 
-  eleventyConfig.addPassthroughCopy('./src/img/');
-  eleventyConfig.addWatchTarget('./src/img/');
+    let markdownIt = require('markdown-it')(options)
+        .use(require('markdown-it-bracketed-spans'))
+        .use(require('markdown-it-attrs'))
 
-  eleventyConfig.addPairedShortcode('year', function (date) {
-    const currentYear = new Date().getFullYear();
-    let result = currentYear - date;
-    result.toString();
-    return `${ result }`;
-  });
+    eleventyConfig.setLibrary('md', markdownIt)
 
-  eleventyConfig.addCollection("navigation", function(collectionApi) {
-    return collectionApi.getFilteredByGlob(["./src/*.md", "./src/*.njk"]);
-  });
+    eleventyConfig.addPassthroughCopy('./src/css/')
+    eleventyConfig.addWatchTarget('./src/css/')
 
-  return {
-    dir: {
-      input: 'src',
-      output: '_site',
-    },
-    templateFormats: [ "md", "njk", "html", ],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
-  }
+    eleventyConfig.addPassthroughCopy('./src/img/')
+    eleventyConfig.addWatchTarget('./src/img/')
+
+    eleventyConfig.addPairedShortcode('year', function (date) {
+        const currentYear = new Date().getFullYear()
+        let result = currentYear - date
+        result.toString()
+        return `${result}`
+    })
+
+    eleventyConfig.addCollection('navigation', function (collectionApi) {
+        return collectionApi.getFilteredByGlob(['./src/*.md', './src/*.njk'])
+    })
+
+    return {
+        dir: {
+            input: 'src',
+            output: '_site',
+        },
+        templateFormats: ['md', 'njk', 'html'],
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+    }
 }
--- src/index.md
+++ src/index.md
@@ -3,7 +3,5 @@ title: home
 layout: 'base.html'
 ---
 
-My name is Amy Churchwell. I’m a {{ resume.title }}. I’ve been working
-at {{ resume.experience[0].company }} since {{ resume.experience[0].startDate }}
-(approximately {% year %}{{ resume.experience[0].startDate }}{% endyear %} years).
-
+Hello! My name is [Amy Churchwell]{.p-name} ([she]{.p-pronoun-nominative}/[her]{.p-pronoun-oblique}).
+I work as a [{{ resume.title }}]{.p-job-title}, which I’ve been doing professionally for approximately {% year %}2017{% endyear %} years. Currently I work on {{ resume.experience[0].projects[0].title }} for {{ resume.experience[0].company }}.
